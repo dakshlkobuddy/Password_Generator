0d35256966790ffba6c178e5fe0f3591e5c85fa0
dakshlkobuddy <91004651+dakshlkobuddy@users.noreply.github.com>
2022-09-05 23:51:02 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbols = "!@#$%^&*()?><;:{}[]\|-_+=.?,"
+
+Use_for = lower_case + upper_case + number + symbols
+
+length_for_pass = 8
+
+password = "".join(random.sample(Use_for, length_for_pass))
+
+print("Your Generated Password is: ", password)
